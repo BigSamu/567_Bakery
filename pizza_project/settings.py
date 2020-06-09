e6044d90be0e996175fec3297bf6c646cfbd8e92
BigSamu <valdesgutierrez@gmail.com>
2020-06-09 17:06:50 -0400

    Initial Design - 567 Bakery

--- pizza_project/settings.py
+++ pizza_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'menu_app',
+    'log_reg_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
